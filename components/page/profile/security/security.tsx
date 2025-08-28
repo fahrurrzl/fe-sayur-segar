ab9e1529f5370635f23f9353e38ad3c055304252
fahrurrzl <fahrurrzl5798@gmail.com>
2025-08-28 15:12:42 +0700

    feat: change password

--- components/page/profile/security/security.tsx
+++ components/page/profile/security/security.tsx
@@ -2,11 +2,27 @@
 
 import React from "react";
 import { Button, Card, CardBody, CardHeader, Input } from "@heroui/react";
-import { FaKey, FaUserShield } from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaKey, FaUserShield } from "react-icons/fa";
 import { useRouter } from "next/navigation";
+import useChangePassword from "@/hooks/useChangePassword";
+import { Controller } from "react-hook-form";
 
 const Security = () => {
   const router = useRouter();
+  const {
+    control,
+    handleSubmit,
+    errors,
+    handleChangePassword,
+    isPendingChangePassword,
+    // visibility
+    isVisibleOldPassword,
+    setIsVisibleOldPassword,
+    isVisibleNewPassword,
+    setIsVisibleNewPassword,
+    isVisibleConfirmPassword,
+    setIsVisibleConfirmPassword,
+  } = useChangePassword();
 
   return (
     <div className="lg:col-span-2">
@@ -22,36 +38,119 @@ const Security = () => {
         </CardHeader>
 
         <CardBody>
-          <form onSubmit={() => {}} className="space-y-8">
+          <form
+            onSubmit={handleSubmit(handleChangePassword)}
+            className="space-y-8"
+          >
             {/* Basic Information */}
             <div className="space-y-6">
               <div>
-                <div className="grid grid-cols-1 gap-6">
-                  <Input
+                <div className="grid grid-cols-1 gap-4">
+                  <Controller
                     name="oldPassword"
-                    onChange={() => {}}
-                    label="Kata Sandi Lama"
-                    placeholder="Masukkan kata sandi lama"
-                    startContent={<FaKey className="h-4 w-4 text-gray-400" />}
-                    isRequired
+                    control={control}
+                    render={({ field }) => (
+                      <Input
+                        {...field}
+                        label="Kata Sandi Lama"
+                        placeholder="Masukkan kata sandi lama"
+                        startContent={
+                          <FaKey className="h-4 w-4 text-gray-400" />
+                        }
+                        variant="bordered"
+                        isInvalid={!!errors.oldPassword}
+                        errorMessage={errors.oldPassword?.message}
+                        type={isVisibleOldPassword ? "text" : "password"}
+                        endContent={
+                          <button
+                            aria-label="toggle password visibility"
+                            className="focus:outline-solid outline-transparent"
+                            type="button"
+                            onClick={() =>
+                              setIsVisibleOldPassword(!isVisibleOldPassword)
+                            }
+                          >
+                            {isVisibleOldPassword ? (
+                              <FaEyeSlash className="text-2xl text-default-400 pointer-events-none" />
+                            ) : (
+                              <FaEye className="text-2xl text-default-400 pointer-events-none" />
+                            )}
+                          </button>
+                        }
+                      />
+                    )}
                   />
 
-                  <Input
-                    name="password"
-                    onChange={() => {}}
-                    label="Kata Sandi Baru"
-                    placeholder="Masukkan kata sandi baru"
-                    startContent={<FaKey className="h-4 w-4 text-gray-400" />}
-                    isRequired
+                  <Controller
+                    name="newPassword"
+                    control={control}
+                    render={({ field }) => (
+                      <Input
+                        {...field}
+                        label="Kata Sandi Baru"
+                        placeholder="Masukkan kata sandi baru"
+                        startContent={
+                          <FaKey className="h-4 w-4 text-gray-400" />
+                        }
+                        variant="bordered"
+                        isInvalid={!!errors.newPassword}
+                        errorMessage={errors.newPassword?.message}
+                        type={isVisibleNewPassword ? "text" : "password"}
+                        endContent={
+                          <button
+                            aria-label="toggle password visibility"
+                            className="focus:outline-solid outline-transparent"
+                            type="button"
+                            onClick={() =>
+                              setIsVisibleNewPassword(!isVisibleNewPassword)
+                            }
+                          >
+                            {isVisibleNewPassword ? (
+                              <FaEyeSlash className="text-2xl text-default-400 pointer-events-none" />
+                            ) : (
+                              <FaEye className="text-2xl text-default-400 pointer-events-none" />
+                            )}
+                          </button>
+                        }
+                      />
+                    )}
                   />
 
-                  <Input
+                  <Controller
                     name="confirmPassword"
-                    onChange={() => {}}
-                    label="Konfirmasi Kata Sandi Baru"
-                    placeholder="Masukkan konfirmasi kata sandi baru"
-                    startContent={<FaKey className="h-4 w-4 text-gray-400" />}
-                    isRequired
+                    control={control}
+                    render={({ field }) => (
+                      <Input
+                        {...field}
+                        label="Konfirmasi Kata Sandi Baru"
+                        placeholder="Masukkan konfirmasi kata sandi baru"
+                        startContent={
+                          <FaKey className="h-4 w-4 text-gray-400" />
+                        }
+                        variant="bordered"
+                        isInvalid={!!errors.confirmPassword}
+                        errorMessage={errors.confirmPassword?.message}
+                        type={isVisibleConfirmPassword ? "text" : "password"}
+                        endContent={
+                          <button
+                            aria-label="toggle password visibility"
+                            className="focus:outline-solid outline-transparent"
+                            type="button"
+                            onClick={() =>
+                              setIsVisibleConfirmPassword(
+                                !isVisibleConfirmPassword
+                              )
+                            }
+                          >
+                            {isVisibleConfirmPassword ? (
+                              <FaEyeSlash className="text-2xl text-default-400 pointer-events-none" />
+                            ) : (
+                              <FaEye className="text-2xl text-default-400 pointer-events-none" />
+                            )}
+                          </button>
+                        }
+                      />
+                    )}
                   />
                 </div>
               </div>
@@ -59,15 +158,12 @@ const Security = () => {
 
             {/* Action Buttons */}
             <div className="flex gap-4 pt-6">
-              <Button
-                type="button"
-                variant="bordered"
-                className="flex-1"
-                onPress={() => router.back()}
-              >
+              <Button type="button" variant="bordered" className="flex-1">
                 Batal
               </Button>
               <Button
+                isLoading={isPendingChangePassword}
+                disabled={isPendingChangePassword}
                 type="submit"
                 color="success"
                 className="flex-1 text-white"
